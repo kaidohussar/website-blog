d13a2bb60739bca93a2b5aba9a3fbe363230827d
kaidohussar <kaido@onoffapp.com>
2021-07-26 19:47:02 +0300

    up

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -2,12 +2,10 @@ import { GetStaticProps } from "next";
 import Layout from "../../components/Layout";
 import BasicMeta from "../../components/meta/BasicMeta";
 import OpenGraphMeta from "../../components/meta/OpenGraphMeta";
-import TwitterCardMeta from "../../components/meta/TwitterCardMeta";
 import PostList from "../../components/PostList";
 import config from "../../lib/config";
 import { countPosts, listPostContent, PostContent } from "../../lib/posts";
 import { listTags, TagContent } from "../../lib/tags";
-import Head from "next/head";
 
 type Props = {
   posts: PostContent[];
@@ -24,7 +22,6 @@ export default function Index({ posts, tags, pagination }: Props) {
     <Layout>
       <BasicMeta url={url} title={title} />
       <OpenGraphMeta url={url} title={title} />
-      <TwitterCardMeta url={url} title={title} />
       <PostList posts={posts} tags={tags} pagination={pagination} />
     </Layout>
   );
