ac4c4eb1c52c670b7c3c109832a689a9fe41a518
kaidohussar <kaido@onoffapp.com>
2021-07-28 22:45:38 +0300

    up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,7 +3,25 @@ import { AppProps } from "next/app";
 // NOTE: Do not move the styles dir to the src.
 // They are used by the Netlify CMS preview feature.
 import "../../public/styles/global.css";
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *::before,
+  *::after {
+    box-sizing: inherit;
+  }
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,19 @@ import Layout from "../components/Layout";
 import BasicMeta from "../components/meta/BasicMeta";
 import OpenGraphMeta from "../components/meta/OpenGraphMeta";
 import { SocialList } from "../components/SocialList";
+import dynamic from "next/dynamic";
+
+const Button = dynamic(
+  () => import("kaidohussar-ui").then((mod) => mod.Button),
+  { ssr: false }
+);
 
 export default function Index() {
   return (
     <Layout>
       <BasicMeta url={"/"} />
       <OpenGraphMeta url={"/"} />
+
       <div className="container">
         <div>
           <h1>
@@ -15,6 +22,9 @@ export default function Index() {
           </h1>
           <span className="handle">@kaidohussar</span>
           <h2>Front-end developer and UX designer</h2>
+          <Button appearance="primary" onClick={() => console.log("click")}>
+            Test
+          </Button>
           <SocialList />
         </div>
       </div>
