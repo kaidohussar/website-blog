f5c5e8f78a248d4cd0a24a6782149333d6580e0b
Kaido Hussar <kaidohus@gmail.com>
2021-08-22 18:47:56 +0300

    up

--- next.config.js
+++ next.config.js
@@ -14,6 +14,11 @@ module.exports = {
           test: /\.svg$/,
           use: "@svgr/webpack",
         },
+        {
+          resolve: {
+            modules: [path.resolve(__dirname, "node_modules"), "node_modules"],
+          },
+        },
       ]
     );
     return config;
