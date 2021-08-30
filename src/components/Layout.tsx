6fff1f610409b1ceb3d4caf615279482d853eb24
Kaido Hussar <kaidohus@gmail.com>
2021-08-30 22:58:27 +0300

    up

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -33,6 +33,7 @@ const Layout = ({ children }: Props) => {
   return (
     <>
       <Head>
+        <meta lang="en" />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" />
