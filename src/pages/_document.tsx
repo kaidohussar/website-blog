9d8115708e666c1eac905715de69a893334457f6
Kaido Hussar <kaidohus@gmail.com>
2021-09-25 22:39:17 +0300

    up

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=block"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700"
             rel="preload"
             as="font"
             crossOrigin="anonymous"
