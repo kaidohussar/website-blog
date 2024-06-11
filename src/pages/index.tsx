cb4ae51b7f23f95dfb3b24af20b76341683f0907
Kaido Hussar <kaidohus@gmail.com>
2024-06-11 20:58:25 +0300

    up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,37 +15,39 @@ const Index = () => {
       <BasicMeta url={'/'} />
       <OpenGraphMeta url={'/'} />
 
-      <motion.div
-        animate={{ y: 0, opacity: 1, pointerEvents: 'all' }}
-        initial={{ y: -20, opacity: 0, pointerEvents: 'none', zIndex: 2 }}
-        className={styles.content}
-      >
-        <Box alignItems="center" justifyContent="center">
-          <Box flexDirection="column" justifyContent="center">
-            <Heading type="h1" size="xxxl" weight="bold" lineHeight="xs">
-              Kaido Hussar
-            </Heading>
-            <Heading type="h2" size="lg">
-              <span className={styles.subHeading}>
-                Problem-solver at heart, helping SaaS products get kickstarted
-              </span>
-            </Heading>
+      <div className={styles.contentWrapper}>
+        <motion.div
+          animate={{ y: 0, opacity: 1, pointerEvents: 'all' }}
+          initial={{ y: -20, opacity: 0, pointerEvents: 'none', zIndex: 2 }}
+          className={styles.content}
+        >
+          <Box alignItems="center" justifyContent="center">
+            <Box flexDirection="column" justifyContent="center">
+              <Heading type="h1" size="xxxl" weight="bold" lineHeight="xs">
+                Kaido Hussar
+              </Heading>
+              <Heading type="h2" size="lg">
+                <span className={styles.subHeading}>
+                  Problem-solver at heart, helping SaaS products get kickstarted
+                </span>
+              </Heading>
 
-            <Button
-              onClick={() =>
-                router.push('/services', undefined, { shallow: true })
-              }
-              className={styles.button}
-              appearance="primary"
-            >
-              Check services
-            </Button>
+              <Button
+                onClick={() =>
+                  router.push('/services', undefined, { shallow: true })
+                }
+                className={styles.button}
+                appearance="primary"
+              >
+                Check services
+              </Button>
+            </Box>
           </Box>
-        </Box>
-        <div role="contentinfo" className={styles.socialLinks}>
-          <SocialList />
-        </div>
-      </motion.div>
+          <div role="contentinfo" className={styles.socialLinks}>
+            <SocialList />
+          </div>
+        </motion.div>
+      </div>
     </Layout>
   )
 }
